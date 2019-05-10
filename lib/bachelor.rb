17baaf528c201efe5aa954b70550d4a47b05c96f
Jason Fournier <jmf4nier@hotmail.com>
2019-05-10 16:25:21 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,24 @@
-def get_first_name_of_season_winner(data, season)
-  # code here
+def get_first_name_of_season_winner(hash, season)
+   winner_name = []
+
+   hash.each do |hash_season,contestants|
+     if hash_season == :"#{season}"
+
+     contestants.each do |contestant|
+       #binding.pry
+       if  contestant[:status].downcase == "winner"
+
+
+         winner_name = contestant[:name].split
+         #binding.pry
+       end
+
+     end
+   end
+
+   end
+   #binding.pry
+   return winner_name[0]
 end
 
 def get_contestant_name(data, occupation)
